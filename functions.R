eca1a789eaa5bbe1b360ba0f349475cd10986998
Elenadisa <elenadiazsantiago.92@gmail.com>
2025-05-30 12:45:07 +0200

    change functions

--- functions.R
+++ functions.R
@@ -42,25 +42,6 @@ alpha_diversity_plot <- function(phy_object, group, alfa_metrics, my_comparisons
 #                           ABUNDANCE                                          #
 ################################################################################
 
-plot_abundance_by_taxa <- function(df, taxa, metric, group){
-  plt <- ggplot(df ,aes(x=get(taxa), y=get(metric), group=get(group), fill=get(group))) +
-    geom_bar(colour="black", stat="identity",position="dodge")  +
-    theme_classic() +
-    theme(axis.text.x = element_text(size = 18, angle = 90, hjust = 1, face = "italic"), 
-          legend.title = element_blank(),
-          axis.title=element_text(size=20,face="bold"),
-          axis.text.y=element_text(size=18),
-          legend.text=element_text(size=20),
-          panel.border = element_rect(colour = "black", fill=NA, size=1)) +
-    scale_fill_grey(start = 0, end = 1) +
-    geom_errorbar(aes(ymin=get(metric), ymax=get(metric)+sd),
-                  width=.5,                    # Width of the error bars
-                  position=position_dodge(.9)) 
-  
-  return(plt)
-  
-}
-
 plot_piechart <- function(df, metric, taxa){
   ggplot(df, aes(x="", y=get(metric), fill=get(taxa))) +
     geom_bar(stat="identity", width=1) +
@@ -76,10 +57,8 @@ plot_piechart <- function(df, metric, taxa){
 #                          DIFFERENTIAL ABUNDANCE DESEQ2                       #
 ################################################################################
 
-daa_bar_plot <- function(df, contrast, title){
-  theme_set(theme_bw())
-  
-    #Transform data
+daa_bar_plot <- function(df, contrast, title, col_pal){
+  #Transform data
   # Phylum order
   x = tapply(df$log2FoldChange, df$Phylum, function(x) max(x))
   x = sort(x, TRUE)
@@ -89,18 +68,18 @@ daa_bar_plot <- function(df, contrast, title){
   x = sort(x, TRUE)
   df$Genus = factor(as.character(df$Genus), levels=names(x))
   
-    #Generate plot
-  ggplot(df) +
+  #Generate plot
+  plt <- ggplot(df) +
     geom_col(aes(x = log2FoldChange, y = Genus, fill = Phylum)) + 
     geom_vline(xintercept = 0.0, color = "Black", size = 0.7)  +
     ggtitle(title) +
-    theme(
-      plot.title = element_text(hjust = 0),
-      panel.border = element_rect(colour = "black", fill=NA, size=1),
-      axis.title=element_text(size=18,face="bold"),
-      axis.text=element_text(size=25),
-      legend.text=element_text(size=25)) +
-    theme_minimal()
+    theme_minimal() 
+    
+    if (!identical(col_pal, FALSE)){
+      plt <- plt + scale_fill_manual(values = col_pal) 
+    }
+  
+  return(plt)
 }
 
 ################################################################################
